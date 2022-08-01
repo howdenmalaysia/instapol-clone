689db12ae8041a245d1376581d1317061e3446ca
David Choy <38069452+davidchoy98@users.noreply.github.com>
2022-08-02 00:34:46 +0800

    Updated Installed Dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+require('laravel-mix-imagemin');
 
 /*
  |--------------------------------------------------------------------------
@@ -6,12 +7,53 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.setPublicPath('public')
+    .setResourceRoot('../')
+    .sass('resources/sass/app.scss', 'css/frontend.css')
+    .sass('resources/sass/dashboard/app.scss', 'css/bo.css')
+    .options({
+        processCssUrl: false,
+    })
+    .js('resources/js/app.js', 'app.js')
+    .extract([
+        'jquery',
+        'popper.js',
+        'bootstrap',
+        'axios',
+        'sweetalert2',
+        'loadash'
+    ])
+    .imagemin(
+        'images',
+        {
+            patterns: [
+                { from: 'node_modules/@fortawesome/fontawesome-free/webfonts', to: 'fonts' },
+            ],
+        },
+        {
+            optiong: {
+                optimizationLevel: 5,
+            },
+            jpegtran: null,
+            plugins: [
+                require('imagemin-mozjpeg')({
+                    quality: 100,
+                    progressive: true
+                }),
+            ],
+        }
+    )
+    .sourceMaps();
+
+if(mix.inProduction()) {
+    mix.version();
+} else {
+    mix.webpackConfig({
+        devtool: 'inline-source-map'
+    });
+}
