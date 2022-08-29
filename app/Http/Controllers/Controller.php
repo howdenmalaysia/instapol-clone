0c89fe8e9b826512497b1d8e36d032e3b30084c4
David Choy <38069452+davidchoy98@users.noreply.github.com>
2022-08-30 04:33:20 +0800

    Added Session Object Checker

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function checkMotorSessionObject(Request $request)
+    {
+        if(empty($request->session()->get('motor')) && $request->url() !== route('motor.index')) {
+            return redirect()->route('motor.index');
+        }
+    }
 }
