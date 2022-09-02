69320b76206696b34e3fdc3876a5a096e8f50fe7
David Choy <38069452+davidchoy98@users.noreply.github.com>
2022-09-03 00:13:54 +0800

    Renamed Seeders & Models According to Table Name

--- database/seeders/Motor/InsuranceCompanySeeder.php
+++ database/seeders/Motor/InsuranceCompanySeeder.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders\Motor;
 
-use App\Models\Motor\Company;
+use App\Models\Motor\InsuranceCompany;
 use Illuminate\Database\Seeder;
 
-class CompanySeeder extends Seeder
+class InsuranceCompanySeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -17,98 +17,98 @@ class CompanySeeder extends Seeder
         $companies = [
             [
                 'name' => 'Etiqa Takaful',
-                'logo' => 'etiqa-takaful',
+                'logo' => 'etiqa-takaful.png',
                 'sequence' => 1,
                 'active' => 1,
             ],
             [
                 'name' => 'Etiqa',
-                'logo' => 'etiqa',
+                'logo' => 'etiqa.png',
                 'sequence' => 2,
                 'active' => 1,
             ],
             [
                 'name' => 'AmGeneral',
-                'logo' => 'am',
+                'logo' => 'am.png',
                 'sequence' => 3,
                 'active' => 1,
             ],
             [
                 'name' => 'Zurich Takaful',
-                'logo' => 'zurich-takaful',
+                'logo' => 'zurich-takaful.png',
                 'sequence' => 4,
                 'active' => 1,
             ],
             [
                 'name' => 'Zurich',
-                'logo' => 'zurich',
+                'logo' => 'zurich.png',
                 'sequence' => 5,
                 'active' => 1,
             ],
             [
                 'name' => 'RHB',
-                'logo' => 'rhb',
+                'logo' => 'rhb.png',
                 'sequence' => 6,
                 'active' => 1,
             ],
             [
                 'name' => 'Allianz',
-                'logo' => 'allianz',
+                'logo' => 'allianz.png',
                 'sequence' => 7,
                 'active' => 1,
             ],
             [
                 'name' => 'Tune Protect',
-                'logo' => 'tune',
+                'logo' => 'tune.png',
                 'sequence' => 8,
                 'active' => 1,
             ],
             [
                 'name' => 'P&O Insurance',
-                'logo' => 'pacificorient',
+                'logo' => 'pacificorient.png',
                 'sequence' => 9,
                 'active' => 1,
             ],
             [
                 'name' => 'AIG',
-                'logo' => 'aig',
+                'logo' => 'aig.png',
                 'sequence' => 10,
                 'active' => 1,
             ],
             [
                 'name' => 'Liberty',
-                'logo' => 'liberty',
+                'logo' => 'liberty.png',
                 'sequence' => 11,
                 'active' => 1,
             ],
             [
                 'name' => 'Berjaya Sompo',
-                'logo' => 'sompo',
+                'logo' => 'sompo.png',
                 'sequence' => 12,
                 'active' => 1,
             ],
             [
                 'name' => 'Lonpac',
-                'logo' => 'lonpac',
+                'logo' => 'lonpac.png',
                 'sequence' => 13,
                 'active' => 1,
             ],
             [
                 'name' => 'Tokio Marine',
-                'logo' => 'tokiomania',
+                'logo' => 'tokiomania.png',
                 'sequence' => 14,
                 'active' => 1,
             ],
             [
                 'name' => 'MPIG',
-                'logo' => 'mpig',
+                'logo' => 'mpig.png',
                 'sequence' => 15,
                 'active' => 1,
             ]
         ];
 
         foreach($companies as $company) {
-            Company::updateOrCreate($company);
+            InsuranceCompany::updateOrCreate($company);
         }
     }
 }
