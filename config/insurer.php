59a5516653a7c88ca87d7ff87b5821981c158b0d
David Choy <38069452+davidchoy98@users.noreply.github.com>
2022-09-08 02:33:51 +0800

    Added Config File

---
+++ config/insurer.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'config' => [
+        'pno' => [
+            'id' => 9,
+            'host' => env('PNO_HOST'),
+            'token_host' => env('PNO_TOKEN_HOST'),
+            'agent_code' => env('PNO_AGENT_CODE'),
+            'user_id' => env('PNO_USER_ID'),
+        ]
+    ] 
+];
--- config/setting.php
+++ config/setting.php
@@ -15,5 +15,19 @@ return [
         'bicycle' => env('BICYCLE_URL'),
         'travel' => env('TRAVEL_URL'),
         'doc_pro' => env('DOC_PRO_URL'),
+    ],
+    'id_type' => [
+        'nric_no' => 1,
+        'company_registration_no' => 2
+    ],
+    'response_codes' => [
+        'unsupported_id_types' => 480,
+        'earlier_renewal' => 481,
+        'invalid_insurance_status' => 490,
+        'insurance_record_mismatch' => 491,
+        'total_payable_not_match' => 492
+    ],
+    'howden' => [
+        'short_code' => env('HOWDEN_SHORT_CODE')
     ]
 ];
