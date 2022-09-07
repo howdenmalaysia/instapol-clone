af991c8d9b3404d431d35d283f2dbec2be297e3d
David Choy <38069452+davidchoy98@users.noreply.github.com>
2022-09-08 02:49:09 +0800

    Fix Undefined Variable Issue

--- resources/views/components/instapol-logo.blade.php
+++ resources/views/components/instapol-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset($navy ? 'images/instapol-navy-text.png' : 'images/instapol-white.png') }}" alt="instaPol" width={{ $width }} {!! $attributes !!}>
+<img src="{{ asset(!empty($navy) && $navy ? 'images/instapol-navy-text.png' : 'images/instapol-white.png') }}" alt="instaPol" width={{ $width }} {!! $attributes !!}>
