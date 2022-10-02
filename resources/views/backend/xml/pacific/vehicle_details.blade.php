1ad3421dfeb4c9941ec62adeb0777f85dc1089a3
David Choy <38069452+davidchoy98@users.noreply.github.com>
2022-10-03 07:01:01 +0800

    Updated Vehicle Details XML Template

--- resources/views/backend/xml/pacific/vehicle_details.blade.php
+++ resources/views/backend/xml/pacific/vehicle_details.blade.php
@@ -1,12 +1,11 @@
-<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
-    <Body>
-        <RequestVehicleInfo xmlns="http://tempuri.org/">
-            <!-- Optional -->
-            <reqData>
-                <TokenId>{{ $token }}</TokenId>
-                <NRIC>{{ $ic_number }}</NRIC>
-                <VehicleRegNo>{{ $vehicle_number }}</VehicleRegNo>
-            </reqData>
-        </RequestVehicleInfo>
-    </Body>
-</Envelope>
+<s12:Envelope xmlns:s12='http://schemas.xmlsoap.org/soap/envelope/'>
+  <s12:Body>
+    <ns1:RequestVehicleInfo xmlns:ns1='http://tempuri.org/'>
+      <ns1:reqData>
+        <ns1:TokenId>{{ $token }}</ns1:TokenId>
+        <ns1:NRIC>{{ $id_number }}</ns1:NRIC>
+        <ns1:VehicleRegNo>{{ $vehicle_number }}</ns1:VehicleRegNo>
+      </ns1:reqData>
+    </ns1:RequestVehicleInfo>
+  </s12:Body>
+</s12:Envelope>
