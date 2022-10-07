b68d24e175d32ccae73e806a5da976cde0b4ac3f
David Choy <38069452+davidchoy98@users.noreply.github.com>
2022-10-07 11:25:46 +0800

    Updated to Populate Vehicle Details Upon AJAX Return

--- resources/views/frontend/motor/vehicle_details.blade.php
+++ resources/views/frontend/motor/vehicle_details.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <x-motor-layout id="vehicle-details" current-step="1">
         <x-slot name="content">
-            <form action="{{ route('motor.vehicle-details') }}" method="POST">
+            <form action="{{ route('motor.vehicle-details') }}" method="POST" id="vehicle-details-form">
                 @csrf
                 <div class="row mb-4">
                     <div class="col-12 col-lg-6">
@@ -19,7 +19,7 @@
                                 <p>{{ __('frontend.motor.vehicle_details.car_number') }}</p>
                             </div>
                             <div class="col-6">
-                                <p class="text-decoration-underline">{{ 'VFM9388' }}</p>
+                                <p id="vehicle-number" class="text-decoration-underline"></p>
                             </div>
                         </div>
                         <div class="row">
@@ -27,7 +27,7 @@
                                 <p>{{ __('frontend.motor.vehicle_details.make') }}</p>
                             </div>
                             <div class="col-6">
-                                <p class="text-decoration-underline">{{ 'Toyota' }}</p>
+                                <p id="make" class="text-decoration-underline"></p>
                             </div>
                         </div>
                         <div class="row">
@@ -35,7 +35,7 @@
                                 <p>{{ __('frontend.motor.vehicle_details.model') }}</p>
                             </div>
                             <div class="col-6">
-                                <p class="text-decoration-underline">{{ 'Vios' }}</p>
+                                <p id="model" class="text-decoration-underline"></p>
                             </div>
                         </div>
                         <div class="row">
@@ -43,7 +43,7 @@
                                 <p>{{ __('frontend.motor.vehicle_details.engine_capacity') }}</p>
                             </div>
                             <div class="col-6">
-                                <p class="text-decoration-underline">{{ '1496' }}</p>
+                                <p id="engine-capacity" class="text-decoration-underline"></p>
                             </div>
                         </div>
                         <div class="row">
@@ -51,7 +51,7 @@
                                 <p>{{ __('frontend.motor.vehicle_details.year') }}</p>
                             </div>
                             <div class="col-6">
-                                <p class="text-decoration-underline">{{ '2020' }}</p>
+                                <p id="manufacture-year" class="text-decoration-underline"></p>
                             </div>
                         </div>
                         <div class="row">
@@ -59,7 +59,7 @@
                                 <p>{{ __('frontend.motor.vehicle_details.variant') }}</p>
                             </div>
                             <div class="col-6">
-                                <select name="varaint" data-select></select>
+                                <select id="variant" name="variant" data-select></select>
                             </div>
                         </div>
                     </div>
@@ -74,7 +74,7 @@
                                 <p>{{ __('frontend.motor.vehicle_details.ncd') }}</p>
                             </div>
                             <div class="col-6">
-                                <p class="text-decoration-underline">{{ '0%' }}</p>
+                                <p id="ncd-percentage" class="text-decoration-underline"></p>
                             </div>
                         </div>
                         <div class="row">
@@ -82,7 +82,7 @@
                                 <p>{{ __('frontend.motor.vehicle_details.coverage') }}</p>
                             </div>
                             <div class="col-6">
-                                <p class="text-decoration-underline">{{ 'Comprehensive' }}</p>
+                                <p id="coverage" class="text-decoration-underline"></p>
                             </div>
                         </div>
                         <div class="row">
@@ -91,9 +91,9 @@
                             </div>
                             <div class="col-6">
                                 <p>
-                                    <span class="text-decoration-underline">{{ '05-11-2022' }}</span>
+                                    <span id="inception-date" class="text-decoration-underline"></span>
                                     to
-                                    <span class="text-decoration-underline">{{ '04-11-2023' }}</span>
+                                    <span id="expiry-date" class="text-decoration-underline"></span>
                                 </p>
                             </div>
                         </div>
@@ -101,7 +101,7 @@
                 </div>
                 <div class="row">
                     <div class="col-12 text-center">
-                        <img src="{{ asset('images/manufacturer/' . strtoupper('Toyota') . '.png') }}" alt="Toyota">
+                        <img id="car-make" src="{{ asset('images/manufacturer/' . strtoupper('Toyota') . '.png') }}" alt="Toyota">
                         <p>{{ __('frontend.motor.vehicle_details.my_vehicle_information') }}</p>
                         <a href="{{ route('motor.index') }}" class="btn btn-link text-uppercase text-dark fw-bold">{{ __('frontend.button.back') }}</a>
                         <button type="button" id="btn-continue" class="btn btn-primary text-white text-uppercase fw-bold">{{ __('frontend.button.continue') }}</button>
@@ -123,8 +123,10 @@
 @push('after-scripts')
 <script>
         let products = JSON.parse("{{ json_encode($product_ids) }}");
+        let motor = JSON.parse($('#motor').val());
 
         $(function() {
+            $('#vehicle-details-form').hide();
             fetchData();
             $('#btn-continue').on('click', function() {
 
@@ -133,10 +135,13 @@
 
         function fetchData() {
             swalLoading();
-            let motor = JSON.parse($('#motor').val());
 
-            products.forEach(product_id => {
-                instapol.post("{{ route('motor.api.vehicle-details') }}", {
+            const controller = new AbortController();
+            let calls = [];
+            let selectedVariant = null;
+
+            products.forEach((product_id, key) => {
+                calls[key] = instapol.post("{{ route('motor.api.vehicle-details') }}", {
                     vehicle_number: motor.vehicle_number,
                     postcode: motor.postcode,
                     id_number: motor.policy_holder.id_number,
@@ -144,16 +149,76 @@
                     email: motor.policy_holder.email,
                     phone_number: motor.policy_holder.phone_number,
                     product_id: product_id
+                }, {
+                    signal: controller.signal
                 }).then((response) => {
                     console.log(response);
+
+                    if(!response.data.code) {
+                        let populated = $('#variant option').length > 1;
+                        let singleVariant = response.data.variants.length === 1;
+
+                        if(!populated && !singleVariant) {
+                            populate(response.data, controller);
+
+                            if(selectedVariant) {
+                                controller.abort();
+                            }
+                        }
+                    } else {
+
+                    }
                 }).catch((error) => {
+                    console.log(error);
                     swalAlert(error.response.data, () => {
-                        window.location = "{{ route('motor.index') }}"
+                        setTimeout(() => {
+                            window.location = "{{ route('motor.index') }}"
+                        }, 5000);
                     })
-                    console.log(error.response);
                 });
             });
+        }
 
+        function populate(data) {
+            // Car Make Logo
+            $('#car-make').attr('src', `{{ assets('images/manufacturer/${data.make.toUpperCase()}.png') }}`)
+            $('#car-make').attr('alt', data.make.toUpperCase());
+
+            // Vehicle Details
+            $('#vehicle-number').val(data.vehicle_number);
+            $('#make').val(data.make);
+            $('#model').val(data.model);
+            $('#engine-capacity').val(data.engine_capcity);
+            $('#manufacture-year').val(data.manufacture_year);
+            $('#ncd-percentage').val(data.ncd_percentage);
+            $('#coverage').val(data.coverage);
+            $('#inception-date').val(data.inception_date);
+            $('#expiry-date')val(data.expiry_date);
+
+            motor.vehicle = {
+                make: data.make,
+                model: data.model,
+                engine_capcity: data.engine_capcity,
+                chassis_number: data.chassis_number,
+                engine_number: data.engine_number,
+                manufacture_year: data.manufacture_year,
+                ncd_percentage: data.ncd_percentage,
+                coverage: data.coverage,
+                inception_date: data.inception_date,
+                expiry_date: data.expiry_date,
+                seating_capacity: data.seating_capacity
+            };
+
+            data.variants.forEach((variant) => {
+                let option = new Option(variant.variant, variant.nric, false, false);
+
+                $('#variants').append(option).trigger('change');
+            });
+
+            motor.variants = data.variants;
+
+            $('#motor').val(JSON.stringify('motor'));
+            $('#vehicle-details-form').show();
         }
     </script>
 @endpush
