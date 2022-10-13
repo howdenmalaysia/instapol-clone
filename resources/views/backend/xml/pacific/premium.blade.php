be0658163dfc628f097ac612b4ca15235497b878
David Choy <38069452+davidchoy98@users.noreply.github.com>
2022-10-13 17:13:27 +0800

    Fix Typo & Element in Loop Issue

--- resources/views/backend/xml/pacific/premium.blade.php
+++ resources/views/backend/xml/pacific/premium.blade.php
@@ -6,22 +6,24 @@
                 <altIDNo xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API"></altIDNo>
                 <companyVehicle xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API">{{ $is_company }}</companyVehicle>
                 <coverage xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API">{{ $coverage }}</coverage>
-                <effDate xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API">{{ $effecitve_date }}</effDate>
+                <effDate xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API">{{ $effective_date }}</effDate>
                 <expDate xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API">{{ $expiry_date }}</expDate>
-                @foreach ($extra_cover as $_extra_cover)
+                @if (!empty($extra_cover))
                     <extraCoverage xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API">
-                        <extraCoverageReq>
-                            <coverageId>{{ $_extra_cover->extra_cover_code }}</coverageId>
-                            <sumInsured>{{ $_extra_cover->sum_insured }}</sumInsured>
-                        </extraCoverageReq>
+                        @foreach ($extra_cover as $_extra_cover)
+                                <extraCoverageReq>
+                                    <coverageId>{{ $_extra_cover->extra_cover_code }}</coverageId>
+                                    <sumInsured>{{ $_extra_cover->sum_insured }}</sumInsured>
+                                </extraCoverageReq>
+                        @endforeach
                     </extraCoverage>
-                @endforeach
+                @endif
                 <gender xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API">{{ $gender }}</gender>
                 <insuredAge xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API">{{ $age }}</insuredAge>
                 <maritalStatus xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API">{{ $marital_status }}</maritalStatus>
                 <ncd xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API">{{ $ncd_percentage }}</ncd>
                 <noofNamedDriver xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API">{{ $additional_driver_count }}</noofNamedDriver>
-                <nricNo xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API">{{ $ic_number }}</nricNo>
+                <nricNo xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API">{{ $id_number }}</nricNo>
                 <onHirePurchase xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API">{{ $hire_purchase }}</onHirePurchase>
                 <postcode xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API">{{ $postcode }}</postcode>
                 <refNo xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API">{{ $reference_number }}</refNo>
