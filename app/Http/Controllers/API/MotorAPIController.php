4eabddbf9f83344634f6811e349c660d5d279bc7
David Choy <38069452+davidchoy98@users.noreply.github.com>
2022-10-26 11:44:16 +0800

    Fix Variable Not Found Issue

--- app/Http/Controllers/API/MotorAPIController.php
+++ app/Http/Controllers/API/MotorAPIController.php
@@ -433,12 +433,12 @@ class MotorAPIController extends Controller implements MotorAPIInterface
             // 7. Update or Insert to Insurance Motor PA Table
             // 8. Update or Insert to Insurance Motor Roadtax Table
             if(!empty($motor->roadtax)) {
-                $region = Postcode::with(['state'])->findOrFail($request->postcode)->first()->state->region;
+                $region = Postcode::with(['state'])->findOrFail($input->postcode)->first()->state->region;
 
                 if($region === 'East') {
                     $delivery_type = RoadtaxDeliveryType::where('description', RoadtaxDeliveryType::EM)->first();
                 } else {
-                    if((int) $request->postcode >= 40000 && (int) $request->postcode <= 68100) {
+                    if((int) $input->postcode >= 40000 && (int) $input->postcode <= 68100) {
                         $delivery_type = RoadtaxDeliveryType::where('description', RoadtaxDeliveryType::KV)->first();
                     } else {
                         $delivery_type = RoadtaxDeliveryType::where('description', RoadtaxDeliveryType::OTHERS)->first();
