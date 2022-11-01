264ea8d2a989cd4b822303ffd456c39df7b7d790
David Choy <38069452+davidchoy98@users.noreply.github.com>
2022-11-01 10:23:04 +0800

    Fix Relation Query ID Issue

--- app/Models/Motor/Insurance.php
+++ app/Models/Motor/Insurance.php
@@ -36,7 +36,7 @@ class Insurance extends Model
 
     public function product()
     {
-        return $this->belongsTo(Product::class, 'id', 'product_id');
+        return $this->belongsTo(Product::class, 'product_id', 'id');
     }
 
     public function extra_attribute()
