09421f458c96e72725ecad325e07b866b5b5a836
David Choy <38069452+davidchoy98@users.noreply.github.com>
2022-11-01 10:11:36 +0800

    Fix URL Incorrect Issue

--- resources/lang/en/frontend.php
+++ resources/lang/en/frontend.php
@@ -653,7 +653,7 @@ return [
                 'title' => 'Confirmation',
                 'line_1' => 'I, :insured_name, confirm that:',
                 'line_2' => 'I understand that it is my duty to take reasonable care not to make a misrepresentation in answering the questions in this Application Form and I hereby declare that I have fully and accurately answered the questions above.',
-                'line_3' => 'I have read, understood and accepted that :pds and <a href="{{ route(\'frontend.privacy\') }}">Privacy Policy</a> for this product.',
+                'line_3' => 'I have read, understood and accepted that :pds and <a href="/privacy-policy">Privacy Policy</a> for this product.',
                 'line_4' => 'Please make sure you review the order and personal details before go to payment page.',
                 'line_5' => 'Once you click \'Pay Now\', you cannot change these details.'
             ]
