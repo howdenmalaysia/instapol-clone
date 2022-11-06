51235ca5c36ae7fc9cb77391b50721d49746b5e0
David Choy <38069452+davidchoy98@users.noreply.github.com>
2022-11-07 04:59:53 +0800

    Added Product ID to Insurer Config

--- config/insurer.php
+++ config/insurer.php
@@ -3,11 +3,13 @@
 return [
     'config' => [
         'pno' => [
+            'product_id' => 10,
             'host' => env('PNO_HOST'),
             'agent_code' => env('PNO_AGENT_CODE'),
             'user_id' => env('PNO_USER_ID'),
         ],
         'bsib' => [
+            'product_id' => 16,
             'agent_code' => env('SOMPO_MOTOR_AGENT_CODE'),
             'host' => env('SOMPO_MOTOR_HOST'),
             'client_id' => env('SOMPO_MOTOR_CLIENT_ID'),
