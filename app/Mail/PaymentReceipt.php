d446acba37996506a5db2a40b56226421779bbf5
David Choy <38069452+davidchoy98@users.noreply.github.com>
2022-12-27 16:59:26 +0800

    Added Payment Receipt Email

---
+++ app/Mail/PaymentReceipt.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class PaymentReceipt extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(object $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject((app()->environment('local', 'development') ? '[Dev] ' : '') . 'Order Confirmation #' . $this->data->insurance_code)
+            ->view('backend.emails.payment_receipt')
+            ->with((array) $this->data);
+    }
+}
---
+++ resources/views/backend/emails/includes/footer.blade.php
@@ -0,0 +1,94 @@
+                        <div style="background-color:transparent;">
+                            <div class="block-grid "
+                                style="Margin: 0 auto; min-width: 320px; max-width: 600px; overflow-wrap: break-word; word-wrap: break-word; word-break: break-word; background-color: #002c5b;">
+                                <div style="border-collapse: collapse;display: table;width: 100%;background-color:#002c5b;">
+                                    <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0" style="background-color:transparent;"><tr><td align="center"><table cellpadding="0" cellspacing="0" border="0" style="width:600px"><tr class="layout-full-width" style="background-color:#002c5b"><![endif]-->
+                                    <!--[if (mso)|(IE)]><td align="center" width="600" style=";width:600px; border-top: 0px solid transparent; border-left: 0px solid transparent; border-bottom: 0px solid transparent; border-right: 0px solid transparent;" valign="top"><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 0px; padding-left: 0px; padding-top:5px; padding-bottom:5px;"><![endif]-->
+                                    <div class="col num12"
+                                        style="min-width: 320px; max-width: 600px; display: table-cell; vertical-align: top; width: 600px;">
+                                        <div style="width:100% !important;">
+                                            <!--[if (!mso)&(!IE)]><!-->
+                                            <div
+                                                style="border-top:0px solid transparent; border-left:0px solid transparent; border-bottom:0px solid transparent; border-right:0px solid transparent; padding-top:5px; padding-bottom:5px; padding-right: 0px; padding-left: 0px;">
+                                                <!--<![endif]-->
+                                                <div class="img-container center fixedwidth" align="center"
+                                                    style="padding-right: 0px;padding-left: 0px;">
+                                                    <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr style="line-height:0px"><td style="padding-right: 0px;padding-left: 0px;" align="center"><![endif]-->
+                                                    <div style="font-size:1px;line-height:50px">&nbsp;</div><img class="center fixedwidth"
+                                                        align="center" border="0" src="{{ asset('images/instapol-white.png') }}"
+                                                        alt="Alternate text" title="Alternate text"
+                                                        style="text-decoration: none; -ms-interpolation-mode: bicubic; height: auto; border: 0; width: 100%; max-width: 120px; display: block;"
+                                                        width="120">
+                                                    <div style="font-size:1px;line-height:20px">&nbsp;</div>
+                                                    <!--[if mso]></td></tr></table><![endif]-->
+                                                </div>
+                                                <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 0px; padding-left: 0px; padding-top: 10px; padding-bottom: 25px; font-family: 'Trebuchet MS', Tahoma, sans-serif"><![endif]-->
+                                                <div
+                                                    style="color:#ffffff;font-family:'Montserrat', 'Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Tahoma, sans-serif;line-height:1.2;padding-top:10px;padding-right:0px;padding-bottom:25px;padding-left:0px;">
+                                                    <div
+                                                        style="line-height: 1.2; font-size: 12px; color: #ffffff; font-family: 'Montserrat', 'Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Tahoma, sans-serif; mso-line-height-alt: 14px;">
+                                                        <p
+                                                            style="font-size: 12px; line-height: 1.2; word-break: break-word; text-align: center; mso-line-height-alt: 14px; margin: 0;">
+                                                            <span style="font-size: 12px;"><a style="text-decoration: none; color: #fff;"
+                                                                    href="https://instapol.my/refund-policy" target="_blank"
+                                                                    rel="noopener">Refund Policy</a> &nbsp;|&nbsp; <a
+                                                                    style="text-decoration: none; color: #fff;"
+                                                                    href="https://instapol.my/privacy-policy" target="_blank"
+                                                                    rel="noopener">Privacy Notice</a> &nbsp;|&nbsp; <a
+                                                                    style="text-decoration: none; color: #fff;"
+                                                                    href="https://instapol.my/terms-and-conditions" target="_blank"
+                                                                    rel="noopener">Terms & Conditions</a></span></p>
+                                                    </div>
+                                                </div>
+                                                <!--[if mso]></td></tr></table><![endif]-->
+                                                <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 10px; padding-left: 10px; padding-top: 10px; padding-bottom: 10px; font-family: 'Trebuchet MS', Tahoma, sans-serif"><![endif]-->
+                                                <div
+                                                    style="color:#ffffff;font-family:'Montserrat', 'Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Tahoma, sans-serif;line-height:1.8;padding-top:10px;padding-right:10px;padding-bottom:10px;padding-left:10px;">
+                                                    <div
+                                                        style="line-height: 1.8; font-size: 12px; color: #ffffff; font-family: 'Montserrat', 'Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Tahoma, sans-serif; mso-line-height-alt: 22px;">
+                                                        <p
+                                                            style="font-size: 12px; line-height: 1.8; word-break: break-word; text-align: center; mso-line-height-alt: 22px; margin: 0;">
+                                                            <span style="font-size: 12px;">Howden Insurance Brokers Sdn. Bhd. (197801001023)</span></p>
+                                                    </div>
+                                                </div>
+                                                <!--[if mso]></td></tr></table><![endif]-->
+                                                <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 10px; padding-left: 10px; padding-top: 10px; padding-bottom: 10px; font-family: 'Trebuchet MS', Tahoma, sans-serif"><![endif]-->
+                                                <div
+                                                    style="color:#ffffff;font-family:'Montserrat', 'Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Tahoma, sans-serif;line-height:1.8;padding-top:10px;padding-right:10px;padding-bottom:10px;padding-left:10px;">
+                                                    <div
+                                                        style="line-height: 1.8; font-size: 12px; color: #ffffff; font-family: 'Montserrat', 'Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Tahoma, sans-serif; mso-line-height-alt: 22px;">
+                                                        <p
+                                                            style="font-size: 12px; line-height: 1.8; word-break: break-word; text-align: center; mso-line-height-alt: 22px; margin: 0;">
+                                                            <span style="font-size: 12px;">Howden Takaful Brokers Sdn. Bhd. (formerly known as Malene Insurance Brokers Sdn Bhd) (198001010734)</span></p>
+                                                    </div>
+                                                </div>
+                                                <!--[if mso]></td></tr></table><![endif]-->
+                                                <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 0px; padding-left: 0px; padding-top: 10px; padding-bottom: 60px; font-family: 'Trebuchet MS', Tahoma, sans-serif"><![endif]-->
+                                                <div
+                                                    style="color:#ffffff;font-family:'Montserrat', 'Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Tahoma, sans-serif;line-height:1.2;padding-top:10px;padding-right:0px;padding-bottom:60px;padding-left:0px;">
+                                                    <div
+                                                        style="line-height: 1.2; font-size: 12px; color: #ffffff; font-family: 'Montserrat', 'Trebuchet MS', 'Lucida Grande', 'Lucida Sans Unicode', 'Lucida Sans', Tahoma, sans-serif; mso-line-height-alt: 14px;">
+                                                        <p
+                                                            style="font-size: 12px; line-height: 1.2; word-break: break-word; text-align: center; mso-line-height-alt: 14px; margin: 0;">
+                                                            <span style="font-size: 12px;">Copyright © 2022 Instapol. All rights reserved.</span></p>
+                                                    </div>
+                                                </div>
+                                                <!--[if mso]></td></tr></table><![endif]-->
+                                                <!--[if (!mso)&(!IE)]><!-->
+                                            </div>
+                                            <!--<![endif]-->
+                                        </div>
+                                    </div>
+                                    <!--[if (mso)|(IE)]></td></tr></table><![endif]-->
+                                    <!--[if (mso)|(IE)]></td></tr></table></td></tr></table><![endif]-->
+                                </div>
+                            </div>
+                        </div>
+                    <!--[if (mso)|(IE)]></td></tr></table><![endif]-->
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    <!--[if (IE)]></div><![endif]-->
+    </body>
+</html>
