1c5678bee907fd31f3ef45e64d672ca77ee3bd1f
David Choy <38069452+davidchoy98@users.noreply.github.com>
2023-01-06 13:14:03 +0800

    Fix Add Ons Not Spliced Issue

--- resources/views/frontend/motor/add_ons.blade.php
+++ resources/views/frontend/motor/add_ons.blade.php
@@ -270,7 +270,7 @@
                 $(this).text("{{ __('frontend.button.show_less') }}");
 
                 var additional_add_ons = motor.extra_cover_list;
-                additional_add_ons = additional_add_ons.splice(0, 5);
+                additional_add_ons.splice(0, 5);
 
                 additional_add_ons.forEach((extra) => {
                     $('#add-on-item').append(
