5a7b5b2c1d95fc6cdf14bc087aae6e3c56d13301
David Choy <38069452+davidchoy98@users.noreply.github.com>
2023-01-15 13:40:55 +0800

    [Test] Fix Stepper Not Active

--- resources/views/components/frontend/motor/steps.blade.php
+++ resources/views/components/frontend/motor/steps.blade.php
@@ -5,7 +5,7 @@
                 <div class="col-12 col-lg-10">
                     <div class="bs-stepper-header">
                         @foreach (array_keys(__('frontend.motor.steps')) as $index => $key)
-                            <div class={{ "step" . ($index === $currentStep ? ' active' : '')}}>
+                            <div class={{ "step" . ($index === --$currentStep ? ' active' : '')}}>
                                 <button type="button" class="step-trigger p-0" role="tab" {{ $index > --$currentStep ? 'disabled' : '' }} data-index="{{ $index }}">
                                     <span class="bg-primary bs-stepper-circle align-items-center">{{ ++$index }}</span>
                                     <span class="position-absolute stepper-label">{{ __("frontend.motor.steps.{$key}") }}</span>
