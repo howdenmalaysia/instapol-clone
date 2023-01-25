272f18506f1738100666b69b6f9d2d6eedbd3bb7
David Choy <38069452+davidchoy98@users.noreply.github.com>
2023-01-26 00:54:07 +0800

    [Test] Fix Multiple AJAX Calls Issue

--- resources/views/frontend/motor/compare.blade.php
+++ resources/views/frontend/motor/compare.blade.php
@@ -461,7 +461,7 @@
                     motor.policy_holder.occupation = $('#occupation').val();
                     $('#motor').val(JSON.stringify(motor));
 
-                    await getPremium();
+                    await getPremium([motor.product_id]);
                     $('#product-form').submit();
                 } else {
                     $('#occupation-error').text("{{ __('frontend.motor.compare_page.occupation_error') }}").removeClass('d-none');
@@ -684,7 +684,11 @@
             });
         });
 
-        function getPremium() {
+        function getPremium(ids = []) {
+            if(ids.length > 0) {
+                products = ids;
+            }
+
             if(controller) {
                 controller.abort();
             }
