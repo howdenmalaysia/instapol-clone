ef4ffa2a7b91dcea9bcd7d669ceb2f181a33cdeb
David Choy <38069452+davidchoy98@users.noreply.github.com>
2023-01-27 03:18:18 +0800

    Fix Typo

--- resources/views/backend/xml/pacific/cover_note_submission.blade.php
+++ resources/views/backend/xml/pacific/cover_note_submission.blade.php
@@ -105,7 +105,7 @@
                 <d4p1:StateCode xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API">{{ $state_code }}</d4p1:StateCode>
                 <d4p1:SumInsured xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API">{{ $sum_insured }}</d4p1:SumInsured>
                 <d4p1:TariffPremium xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API">{{ $premium_details->tariff_premium }}</d4p1:TariffPremium>
-                <d4p1:TotalPremium xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API">{{ $total_premium }}</d4p1:TotalPremium>
+                <d4p1:TotalPremium xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API">{{ $premium_details->total_premium }}</d4p1:TotalPremium>
                 <d4p1:TypeOfCoverCode xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API">{{ $type_of_cover }}</d4p1:TypeOfCoverCode>
                 <d4p1:YearMake xmlns="http://schemas.datacontract.org/2004/07/PO.Web.API">{{ $year_make }}</d4p1:YearMake>
             </reqData>
