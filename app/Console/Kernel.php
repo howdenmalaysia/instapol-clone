a8074d028a338426691bf750dab9e7dbaba683c8
David Choy <38069452+davidchoy98@users.noreply.github.com>
2023-02-01 16:02:51 +0800

    Updated Schedule Cron Reports

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,10 @@
 
 namespace App\Console;
 
+use App\Console\Commands\EGHLSettlement;
+use App\Console\Commands\HowdenSettlement;
+use App\Console\Commands\InsurerSettlement;
+use App\Console\Commands\MotorRenewalNotice;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,7 +28,20 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        // 1. Settlement Reports [Wed, Fri]
+        $schedule->command(EGHLSettlement::class)
+            ->dailyAt('08:00')
+            ->days([Schedule::WEDNESDAY, Schedule::FRIDAY]);
+        $schedule->command(InsurerSettlement::class)
+            ->dailyAt('08:05')
+            ->days([Schedule::WEDNESDAY, Schedule::FRIDAY]);
+        $schedule->command(HowdenSettlement::class)
+            ->dailyAt('08:10')
+            ->days([Schedule::WEDNESDAY, Schedule::FRIDAY]);
+            
+        // 2. Motor Renewal Notice [1 Month, 2 Weeks, 1 Week] (Before Expiry)
+        $schedule->command(MotorRenewalNotice::class)->dailyAt('10:00');
+
     }
 
     /**
