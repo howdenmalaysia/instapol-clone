47c677d8b0f0f4f8519ad6bd7bc34a3808445ea0
David Choy <38069452+davidchoy98@users.noreply.github.com>
2023-02-10 16:04:04 +0800

    Fix Add Ons Sum Insured Value Parsed as String Issue

--- resources/views/frontend/motor/add_ons.blade.php
+++ resources/views/frontend/motor/add_ons.blade.php
@@ -550,10 +550,10 @@
 
             // Consolidate Add Ons
             $('.extra-coverage-checkbox:checked').each((index, element) => {
-                let sum_insured = $(`#sum-insured-${$.escapeSelector($(element).val())}`).val();
+                let sum_insured = parseFloat($(`#sum-insured-${$.escapeSelector($(element).val())}`).val());
 
                 if(isNaN($(`#sum-insured-${$.escapeSelector($(element).val())}`).val())) {
-                    sum_insured = $('#sum-insured-slider').val();
+                    sum_insured = parseFloat($('#sum-insured-slider').val());
                 }
 
                 if($(element).val() != 112) { // CART
