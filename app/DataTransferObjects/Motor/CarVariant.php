c9e50c039affb9cbf4e2025ddd10b7855ae3e2e6
howdenmalaysia <71696058+howdenmalaysia@users.noreply.github.com>
2023-03-17 15:24:35 +0800

    pull

---
+++ app/DataTransferObjects/Motor/CarVariant.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\DataTransferObjects\Motor;
+
+use Spatie\DataTransferObject\DataTransferObject;
+
+class CarVariant extends DataTransferObject
+{   
+    /** @var object|null */
+    public $variant;
+}
