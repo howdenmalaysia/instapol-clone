5a122998b12a3277030eda144af40587bbce9e35
james <james@lcpbuildsofttechnology.com>
2023-04-18 15:33:40 +0800

    z & z production credential

--- resources/views/backend/xml/zurich/jpj_status.blade.php
+++ resources/views/backend/xml/zurich/jpj_status.blade.php
@@ -1,6 +1,6 @@
 <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
     <s:Body>
-        <GetJPJStatus xmlns="https://gtws2.zurich.com.my/ziapps/zurichinsurance">
+        <GetJPJStatus xmlns="https://api.zurich.com.my/v1/general/insurance/motor">
             <XmlRequest>
             <![CDATA[<RequestData>
                     <ParticipantDetails>
