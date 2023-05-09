73e1084c9dbdd50ebc69c88d0773204de6dc6c28
David Choy <38069452+davidchoy98@users.noreply.github.com>
2023-05-09 10:13:22 +0800

    Fix Typo

--- app/Console/Commands/Settlement/EGHLSettlement.php
+++ app/Console/Commands/Settlement/EGHLSettlement.php
@@ -160,7 +160,7 @@ class EGHLSettlement extends Command
             Excel::store(new EGHLReportExport($row_data), $filename);
 
             Mail::to(config('setting.settlement.eghl.to'))
-                ->cc(array_merge(config('setting.settlement.eghl.to'), config('setting.howden.affinity_team_email')))
+                ->cc(array_merge(config('setting.settlement.eghl.cc'), config('setting.howden.affinity_team_email')))
                 ->bcc(config('setting.howden.it_dev_mail'))
                 ->send(new EGHLSettlementMail($filename, $start_date, $end_date));
 
