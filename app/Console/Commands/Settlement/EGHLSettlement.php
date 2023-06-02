1b93f34cf292d5dc4222ecf82e25917f0f70964c
David Choy <38069452+davidchoy98@users.noreply.github.com>
2023-06-02 14:26:52 +0800

    Fix EGHL Settlement Not Querying Full Day Record Issue

--- app/Console/Commands/Settlement/EGHLSettlement.php
+++ app/Console/Commands/Settlement/EGHLSettlement.php
@@ -54,7 +54,8 @@ class EGHLSettlement extends Command
     {
         Log::info("[Cron - eGHL Settlement] Start Generating Report.");
 
-        $start_date = $end_date = Carbon::now()->subDay()->format(self::DATETIME_FORMAT);
+        $start_date = Carbon::now()->subDay()->startOfDay()->format(self::DATETIME_FORMAT);
+        $end_date = Carbon::now()->subDay()->endOfDay()->format(self::DATETIME_FORMAT);
         if(!empty($this->argument('start_date')) && !empty($this->argument('end_date'))) {
             $start_date = Carbon::parse($this->argument('start_date'))->startOfDay()->format(self::DATETIME_FORMAT);
             $end_date = Carbon::parse($this->argument('end_date'))->endOfDay()->format(self::DATETIME_FORMAT);
