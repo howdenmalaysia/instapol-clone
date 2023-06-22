49cdfaa474855702ee3485c52ee797707e36a5b3
DzulJamal-DJ <dzul.jamal@my.howdengroup.com>
2023-06-22 10:26:13 +0800

    Updated Motor Insurer Logo Page

--- resources/views/components/frontend/motor/instapol-motor-insurer.blade.php
+++ resources/views/components/frontend/motor/instapol-motor-insurer.blade.php
@@ -1,10 +1,10 @@
 <div class="row">
     <div class="col-12">
-        @foreach ($insurers->chunk(6) as $insurers)
-            <div class="row align-items-center border border-primary border-5 rounded-pill p-2 p-md-3 mb-4 bg-white">
+        @foreach ($insurers->chunk(13) as $insurers)
+            <div class="row align-items-center border border-primary border-5 rounded-pill p-3  mb-4 bg-white">
                 @foreach ($insurers as $insurer)
                     @if (!$insurer->coming_soon)
-                        <div class="col-4 col-sm-3 col-md-2">
+                        <div class="col-4 col-sm-2">
                             <img src="{{ asset("images/insurer/{$insurer->logo}") }}" class="img-fluid">
                         </div>
                     @endif
@@ -12,18 +12,22 @@
             </div>
         @endforeach
     </div>
-    <div class="col-10 d-none d-lg-block">
+    <div class="col-12 d-none d-lg-block">
         <div class="row align-items-center border border-primary border-5 rounded-pill p-3 bg-white">
             <div class="col-2 mt-2">
                 <h4 class="fw-bold text-start text-uppercase">{{ __('frontend.motor.coming_soon') }}</h4>
             </div>
-            @foreach ($insurers as $insurer)
+            <div class="col-10">
+                <div class="row">
+                @foreach ($insurers as $insurer)
                 @if ($insurer->coming_soon)
-                    <div class="col-4 col-sm-3 col-md-2">
+                    <div class="col-4 col-sm-2">
                         <img src="{{ asset("images/insurer/coming-soon/{$insurer->logo}") }}" class="img-fluid">
                     </div>
                 @endif
             @endforeach
+                </div>
+            </div>
         </div>
     </div>
 </div>
