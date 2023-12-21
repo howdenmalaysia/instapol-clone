c2a81f5b5b5d06897c3cb9e18e916e744d0c20d4
DzulJamal-DJ <dzul.jamal@my.howdengroup.com>
2023-12-21 11:05:26 +0800

    Update referral hide menu

--- resources/views/frontend/includes/navbar.blade.php
+++ resources/views/frontend/includes/navbar.blade.php
@@ -1,16 +1,25 @@
 <nav class="navbar navbar-expand-lg navbar-fixed-top bg-primary">
     <div class="container">
+        @if(Session::has('referral'))
+        <div class="navbar-brand">
+        <x-instapol-logo width="115" />
+        </div>
+        @else
         <a href="{{ route('frontend.index') }}" class="navbar-brand">
             <x-instapol-logo width="115" />
         </a>
+        @endif
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-collapse" aria-controls="#navbar-collapse" aria-expanded="false" aria-label="Toggle Navigation Bar">
             <i class="fas fa-bars"></i>
         </button>
         <div id="navbar-collapse" class="collapse navbar-collapse">
             <ul class="navbar-nav ms-auto text-uppercase">
+                @if(Session::has('referral'))
+                @else
                 <li class="nav-item">
                     <a href="/" class="nav-link">{{ __('frontend.navbar.home') }}</a>
                 </li>
+                @endif
                 <li class="nav-item dropdown">
                     <a id="about-dropdown" class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false"><span class="me-1">{{ __('frontend.navbar.about.about') }}</span></a>
                     <ul class="dropdown-menu p-0" aria-labelledby="about-dropdown">
@@ -31,18 +40,27 @@
                         </li>
                     </ul>
                 </li>
+                @if(Session::has('referral'))
+                @else
                 <li class="nav-item">
                     <a href="" class="nav-link">{{ __('frontend.navbar.promotion') }}</a>
                 </li>
+                @endif
                 <li class="nav-item">
                     <a href="{{ route('frontend.claims') }}" class="nav-link">{{ __('frontend.navbar.claims') }}</a>
                 </li>
+                @if(Session::has('referral'))
+                @else
                 <li class="nav-item">
                     <a href="{{ route('instapol_blog') }}" class="nav-link">{{ __('frontend.navbar.blog') }}</a>
                 </li>
+                @endif
+                @if(Session::has('referral'))
+                @else
                 <li class="nav-item">
-                    <a href="{{ route('dashboard') }}" class="nav-link">{{ __('frontend.navbar.login') }}</a>
+                    <a href="{{ route('instapol_blog') }}" class="nav-link">{{ __('frontend.navbar.blog') }}</a>
                 </li>
+                @endif
             </ul>
         </div>
     </div>
