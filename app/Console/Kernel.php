501d9ee2ffea570cd3afd70b14c56190acda947f
DzulJamal-DJ <dzul.jamal@my.howdengroup.com>
2024-01-16 14:00:41 +0800

    Update Kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -37,21 +37,18 @@ class Kernel extends ConsoleKernel
         /// a. eGHL Settlement
         // $schedule->command(EGHLSettlement::class)
         //     ->dailyAt('10:00');
-        //$schedule->command(EGHLSettlement::class)->dailyAt('08:15')->dailyAt('16:15');
-        $schedule->command(EGHLSettlement::class)->dailyAt('12:35');
-
+        $schedule->command(EGHLSettlement::class)->dailyAt('08:15');
+        $schedule->command(EGHLSettlement::class)->dailyAt('16:15');
         /// b. Insurers Settlement
         // $schedule->command(InsurerSettlement::class)
         //     ->dailyAt('10:05');
-        //$schedule->command(InsurerSettlement::class)->dailyAt('08:05')->dailyAt('16:05');
-        $schedule->command(InsurerSettlement::class)->dailyAt('12:30');
-
+        $schedule->command(InsurerSettlement::class)->dailyAt('08:05');
+        $schedule->command(InsurerSettlement::class)->dailyAt('16:05');
         /// c. Howden Internal Settlement
         // $schedule->command(HowdenSettlement::class)
         //     ->dailyAt('10:10');
-        //$schedule->command(HowdenSettlement::class)->dailyAt('08:10')->dailyAt('16:10');
-        $schedule->command(HowdenSettlement::class)->dailyAt('12:33');
-
+        $schedule->command(HowdenSettlement::class)->dailyAt('08:10');
+        $schedule->command(HowdenSettlement::class)->dailyAt('16:10');
 
         /// d. Monthly Howden Internal Settlement [First Business Day of Each Month]
         $schedule->command(MonthlySettlement::class)
